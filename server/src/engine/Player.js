2fea30a217667736d331f3cc7472f7ebf865e89c
amshumon <148518008+amshumon@users.noreply.github.com>
2026-05-09 01:59:13 +0530

    Add files via upload

---
+++ server/src/engine/Player.js
@@ -0,0 +1,39 @@
+const crypto = require('crypto');
+
+class Player {
+  constructor(socketId, name, avatar = 'default', isBot = false) {
+    this.id = crypto.randomUUID();
+    this.socketId = socketId; // null for bots
+    this.name = name;
+    this.avatar = avatar;
+    this.isBot = isBot;
+    this.hand = [];
+    this.isReady = false;
+    this.connected = true;
+    this.score = 0;
+    this.hasSaidUno = false;
+    this.isFrozen = false;
+  }
+
+  addCards(cards) {
+    this.hand.push(...cards);
+  }
+
+  removeCard(cardId) {
+    const index = this.hand.findIndex(c => c.id === cardId);
+    if (index !== -1) {
+      return this.hand.splice(index, 1)[0];
+    }
+    return null;
+  }
+
+  hasCard(cardId) {
+    return this.hand.some(c => c.id === cardId);
+  }
+
+  getHandSize() {
+    return this.hand.length;
+  }
+}
+
+module.exports = { Player };
